2db2f44a20535283570116b0f132ac0953952b42
Nithin Bekal <nithinbekal@gmail.com>
2016-01-20 17:51:02 +0530

    Add the views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :messages, only: [:index]
+  resources :sessions, only: [:new, :create]
 
-  # Serve websocket cable requests in-process
+  root 'sessions#new'
+
   # mount ActionCable.server => '/cable'
 end
